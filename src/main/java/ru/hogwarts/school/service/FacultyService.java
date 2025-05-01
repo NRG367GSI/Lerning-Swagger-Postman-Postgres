fa318d06ad7fae37643f5e85f1809b55275e3114
Gavrilov-S <3672010@mail.ru>
2025-05-01 18:36:06 +0300

    end work

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.List;
 
