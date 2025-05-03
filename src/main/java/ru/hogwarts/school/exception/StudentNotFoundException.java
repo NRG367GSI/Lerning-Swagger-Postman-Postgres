8924d5dc869dffff1c4f3d209ab6dcc978615dca
Gavrilov-S <3672010@mail.ru>
2025-05-03 15:23:54 +0300

    deleted toSting

--- src/main/java/ru/hogwarts/school/exception/StudentNotFoundException.java
+++ src/main/java/ru/hogwarts/school/exception/StudentNotFoundException.java
@@ -4,9 +4,4 @@ public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
         super(message);
     }
-
-    @Override
-    public String toString() {
-        return "StudentNotFoundException{}";
-    }
 }
